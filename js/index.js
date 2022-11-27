d651e447c68c3c37301e2e27c75680ab89885fad
gs-sangeeth <gssangeeth16@gmail.com>
2022-11-27 13:03:20 +0530

    add 3d renders

--- js/index.js
+++ js/index.js
@@ -11,106 +11,96 @@ navLinks.forEach((link) => {
   });
 });
 
-// let width, height;
-// let pixels = [];
-// let coloredPixels = [];
-// let colors = ["#540045", "#C60052", "#FF714B", "#EAFF87", "#ACFFE9"];
-// let currentPixel = 0;
-// const mousePosition = { x: window.innerWidth / 2, y: window.innerHeight / 2 };
-
-// const canvas = document.getElementById("canvas");
-// const ctx = canvas.getContext("2d");
-// const theDiv = document.getElementById("start");
-
-// const drawGrid = () => {
-//   ctx.clearRect(0, 0, width, height);
-
-//   for (var i = 0, l = pixels.length; i < l; i++) {
-//     pixels[i][4] = 0;
-//   }
-
-//   for (var i = 0, l = coloredPixels.length; i < l; i++) {
-//     var pix =
-//       Math.floor(coloredPixels[i].y / 10) * (Math.floor(width / 10) + 1) +
-//       Math.floor(coloredPixels[i].x / 10);
-//     if (pixels[pix]) {
-//       pixels[pix][4] = coloredPixels[i].color;
-//       pixels[pix][5] = coloredPixels[i].alpha;
-//     }
-
-//     if (coloredPixels[i].alpha > 0) coloredPixels[i].alpha -= 0.008;
-//     if (coloredPixels[i].alpha < 0) coloredPixels[i].alpha = 0;
-//     coloredPixels[i].x += coloredPixels[i].vx;
-//     coloredPixels[i].y += coloredPixels[i].vy;
-//   }
-
-//   for (var i = 0, l = pixels.length; i < l; i++) {
-//     ctx.globalAlpha = 1;
-//     ctx.fillStyle = "#222";
-//     ctx.fillRect(pixels[i][0], pixels[i][1], pixels[i][2], pixels[i][3]);
-//     ctx.globalAlpha = pixels[i][5];
-//     ctx.fillStyle = pixels[i][4];
-//     ctx.fillRect(pixels[i][0], pixels[i][1], pixels[i][2], pixels[i][3]);
-//   }
-// };
-
-// const resize = () => {
-//   width = theDiv.clientWidth;
-//   height = theDiv.clientHeight;
-//   canvas.width = width;
-//   canvas.height = height;
-//   pixels = [];
-//   for (var y = 0; y < height / 10; y++) {
-//     for (var x = 0; x < width / 10; x++) {
-//       pixels.push([x * 10, y * 10, 8, 8, "#222", 1]);
-//     }
-//   }
-// };
-
-// const draw = () => {
-//   launchPixel();
-//   launchPixel();
-//   drawGrid();
-//   requestAnimationFrame(draw);
-// };
-
-// const initColoredPixels = () => {
-//   for (var i = 0; i < 300; i++) {
-//     coloredPixels.push({
-//       x: width / 2,
-//       y: height / 2,
-//       alpha: 0,
-//       color: colors[i % 5],
-//       vx: -1 + Math.random() * 2,
-//       vy: -1 + Math.random() * 2,
-//     });
-//   }
-// };
-
-// const launchPixel = () => {
-//   coloredPixels[currentPixel].x = mousePosition.x;
-//   coloredPixels[currentPixel].y = mousePosition.y;
-//   coloredPixels[currentPixel].alpha = 1;
-
-//   currentPixel++;
-//   if (currentPixel > 299) currentPixel = 0;
-// };
-
-// resize();
-// initColoredPixels();
-// draw();
-
-// window.addEventListener("resize", resize);
-// window.addEventListener("mousemove", function (e) {
-//   mousePosition.x = e.pageX;
-//   mousePosition.y = e.pageY;
-// });
-
-// const touchMove = (e) => {
-//   e.preventDefault();
-//   mousePosition.x = e.touches[0].pageX;
-//   mousePosition.y = e.touches[0].pageY;
-// };
-
-// document.addEventListener("touchstart", touchMove);
-// document.addEventListener("touchmove", touchMove);
+(function() {
+	
+	//Declare function: createArcadeCharacter
+	//n: amount of columns
+	//cw: width of a bit or cell
+	//ch: height of a bit or cell
+	function createArcadeCharacter(n, cw, ch) {
+		//Variables
+		var tempStr = '', t = 0, l = 0, b = 0;
+		//Create pattern
+		var pattern = createArcadeCharacterPattern(n);
+		//Loop through bits, iterate n*n times
+		for(var i = 0;i < (n * n);i++) {
+			b = pattern.charAt(i);
+			tempStr += '<div class="bit';	
+			//Check if bit is active or on
+			if(b == 1) {
+				tempStr += ' bit--active';
+			}
+			tempStr += '"';
+			tempStr += ' style="';
+			tempStr += 'top:' + t + 'px;';
+			tempStr += 'left:' + l + 'px;';
+			tempStr += 'width:' + cw + 'px;';
+			tempStr += 'height:' + ch + 'px;';
+			tempStr += '">';
+			tempStr += '</div>';
+			
+			if ((i > 0) && (i + 1) % n == 0) {
+				t = 0;
+				l += cw;
+			} else {
+				t += ch;
+			}
+			
+		}
+		//Return Arcade Character As String
+		return tempStr;
+	}	
+	
+	//Declare function: createArcadeCharacterPattern
+	//Create random Arcade Character Pattern
+	//n: amount of columns
+	function createArcadeCharacterPattern(n) {
+		var patternStr = '', reflectionPatterStr = '', patternStrLength = Math.ceil(n / 2) * n;
+		for(var i = 0;i < patternStrLength;i++) {
+			patternStr += Math.round(Math.random());
+		}
+		for(var i = 0;i < (patternStrLength / n) - 1;i++) {
+			reflectionPatterStr = patternStr.substring((i * n), (i * n) + n) + reflectionPatterStr;
+		}
+		return patternStr + reflectionPatterStr;
+	}
+	
+	//Declare function: createArcadeCharacterWall
+	//Create Arcade Characters Wall
+	//n: amount of characters
+	//an: amount of columns per character
+	//bw: width of a bit
+	//bh: height of a bit
+	function createArcadeCharacterWall(n, an, bw, bh) {
+		//Get the .arcade container (html div element)
+		var container = document.querySelector('.arcadewall');
+		//Variables
+		var tempStr = '', acw = bw*an, ach = bh*an, at = 0, al = 0;
+		//Loop through bits, iterate n*n times
+		for(var i = 0;i < n;i++) {
+			tempStr += '<div class="arcade"';
+			tempStr += ' style="';
+			tempStr += ' width:' + acw + 'px;';
+			tempStr += ' height:' + ach + 'px;';
+			tempStr += ' margin:' + bw + 'px;';
+			tempStr += '">';
+			tempStr += createArcadeCharacter(an, bw, bh);
+			tempStr += '</div>';
+		}
+		//Write To The DOM
+		container.innerHTML = tempStr;
+	}
+	
+	function updateArcadeChracterWall() {
+		//Callback function: createArcadeCharacterWall
+		//Don't forget the value for the arguments
+		createArcadeCharacterWall(104, 7, 7, 7);
+		
+		//Animate Arcade Characters Wall
+		window.setTimeout(function() { updateArcadeChracterWall(); }, 1500);
+	}
+	
+	//Callback function: updateArcadeChracterWall
+	updateArcadeChracterWall();
+	
+})();
